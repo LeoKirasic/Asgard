0982e30469d30dabcd8ca93164236e03d24ce574
Leo Kirasic <kirasicleo@gmail.com>
2022-04-08 13:40:35 +0200

    Add uiConfig const

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,7 +11,20 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
 });
 
+export const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+
 firebase.firestore();
-export const db = app.firestore();
+
 export const auth = app.auth();
+export const db = app.firestore();
 export default app;
