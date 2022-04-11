c68b4b9272a8314ef5a4f97ef58fde366484a72d
Leo Kirasic <kirasicleo@gmail.com>
2022-04-11 17:56:33 +0200

    Render Signup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Signup from './components/Signup';
 import './App.css';
 
 function App() {
-  return <div className=" text-green-500">Asgard</div>;
+  return (
+    <div className=" text-green-500">
+      Asgard
+      <Signup></Signup>
+    </div>
+  );
 }
 
 export default App;
