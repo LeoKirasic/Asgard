d042e0dfe20664d372bcbbfc1ba108de4b136e1b
Leo Kirasic <kirasicleo@gmail.com>
2022-04-11 17:57:31 +0200

    Add signIn callback

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,13 +14,15 @@ const app = firebase.initializeApp({
 export const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
   ],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
 };
 
 firebase.firestore();
