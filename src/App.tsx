fe50752283cc672bcda63f8060546e9873495a2f
Leo Kirasic <kirasicleo@gmail.com>
2022-04-14 16:56:30 +0200

    Wrap components in AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Signup from './components/Signup';
-import './App.css';
-
+import Chat from './components/Chat';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthProvider from './context/AuthContext';
+import Header from './components/Header';
 function App() {
   return (
-    <div className=" text-green-500">
-      Asgard
-      <Signup></Signup>
+    <div className="flex flex-col items-center bg-background2 ml-6 mr-6 rounded-md">
+      <Header></Header>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/" element={<ProtectedRoute outlet={<Chat />} />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
