895ffc7381fde1537354939f63739c4d05a7d140
Leo Kirasic <kirasicleo@gmail.com>
2022-04-18 16:24:50 +0200

    Remove unused imports

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Chat from './Chat';
 import { useAuth } from '../context/AuthContext';
-import firebase from 'firebase/compat/app';
 import Signup from './Signup';
 interface ProtectedRouteProps {
   outlet: any;
