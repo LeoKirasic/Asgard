8c449dad1652bdc5d5eee1a746f64550d0ee2a9f
Leo Kirasic <kirasicleo@gmail.com>
2022-04-22 14:57:12 +0200

    Sort query by timestamp

--- src/helpers/getMessages.ts
+++ src/helpers/getMessages.ts
@@ -1,15 +1,19 @@
 import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 
 async function getMessages() {
-  const colRef = collection(db, 'messages');
+  const messagesRef = collection(db, 'messages');
+  const messagesQuery = query(
+    messagesRef,
+    orderBy('timestamp', 'desc'),
+    limit(10)
+  );
   let messages: Array<object> = [];
   return new Promise((resolve, reject) => {
-    getDocs(colRef).then((snapshot) => {
+    getDocs(messagesQuery).then((snapshot) => {
       snapshot.docs.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
-      messages.sort((a, b) => (a as any).timestamp - (b as any).timestamp);
       resolve(messages);
     });
   });
