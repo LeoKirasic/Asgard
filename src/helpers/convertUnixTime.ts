62d69aeac9bb9d51139c8490fff9e492a80f8e5f
Leo Kirasic <kirasicleo@gmail.com>
2022-04-26 18:57:21 +0200

    Remove seconds

--- src/helpers/convertUnixTime.ts
+++ src/helpers/convertUnixTime.ts
@@ -2,10 +2,8 @@ function convertUnixTime(unixTimestamp: number) {
   const date = new Date(unixTimestamp * 1000);
   const hours = date.getHours();
   const minutes = '0' + date.getMinutes();
-  const seconds = '0' + date.getSeconds();
 
-  const formattedTime =
-    hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  const formattedTime = hours + ':' + minutes.substr(-2);
 
   return formattedTime;
 }
