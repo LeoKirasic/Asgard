eb11db098dc8e014792caa5cc43c9cc3f8039fb4
Leo Kirasic <kirasicleo@gmail.com>
2022-04-27 17:09:58 +0200

    Add additional screen values

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         background2: '#1E1E1E',
         backgroundDarker: '#111111',
       },
+      screens: {
+        xs: { max: '420px' },
+        '2xs': { max: '365px' },
+      },
     },
   },
   plugins: [],
