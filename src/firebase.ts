3cd456e794b4fc76ed4e046ab11695d22a46a797
Leo Kirasic <kirasicleo@gmail.com>
2022-04-27 18:56:04 +0200

    Remove comments

--- src/firebase.ts
+++ src/firebase.ts
@@ -12,15 +12,12 @@ const app = firebase.initializeApp({
 });
 
 export const uiConfig = {
-  // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
-  // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
   ],
   callbacks: {
-    // Avoid redirects after sign-in.
     signInSuccessWithAuthResult: () => false,
   },
 };
