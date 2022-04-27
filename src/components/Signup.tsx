3f6b2cad703b65a716a53dd63b8127a40d06c4e5
Leo Kirasic <kirasicleo@gmail.com>
2022-04-27 18:53:50 +0200

    Remove unused import

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
