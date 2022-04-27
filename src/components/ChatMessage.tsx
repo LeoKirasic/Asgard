76d9036a877dc18674224a8545b012c2439880d9
Leo Kirasic <kirasicleo@gmail.com>
2022-04-27 17:05:54 +0200

    Add additional information to messages

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import convertUnixTime from '../helpers/convertUnixTime';
+
+interface DateProperties {
+  seconds: number;
+  nanoseconds: number;
+}
 
 interface ChatProps {
   id: string;
-  timestamp: Date;
+  timestamp: DateProperties;
   name: string;
   text: string;
   profilePicUrl: string;
 }
 
 function ChatMessage(props: ChatProps) {
+  const time = convertUnixTime(props.timestamp.seconds);
   return (
-    <div>
-      <img
-        src={
-          props.profilePicUrl ||
-          'https://api.adorable.io/avatars/23/abott@adorable.png'
-        }
-      />
-      <p>{props.text}</p>
+    <div className="flex flex-row items-start border-y border-stone-800 pt-2 pb-2 text-lg 2xl:text-2xl">
+      <div className="flex flex-col mr-2">
+        <p>{props.name}</p>
+        <img
+          className=" w-14 rounded-xl 2xl:w-20"
+          src={
+            props.profilePicUrl ||
+            'https://api.adorable.io/avatars/23/abott@adorable.png'
+          }
+        />
+      </div>
+      <div className="flex flex-col">
+        <div className="text-white text-opacity-50">{time}</div>
+        <p>{props.text}</p>
+      </div>
     </div>
   );
 }
